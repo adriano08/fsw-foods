1de211776dda273fdab745af7d611d812aa7720c
Adriano Carmo <carmoadriano08@gmail.com>
2024-05-02 18:33:06 -0300

    feat: add promo banner to homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,5 @@
+import Image from "next/image";
+import CategoryList from "./components/category-list";
 import Header from "./components/header";
 import Search from "./components/search";
 
@@ -5,8 +7,25 @@ export default function Home() {
   return (
     <>
       <Header />
+
       <div className="px-5 pt-6">
         <Search />
+      </div>
+
+      <div className="px-5 pt-6">
+        <CategoryList />
+      </div>
+
+      <div className="px-5 pt-6">
+        <Image
+          src="/promo-banner-01.png"
+          alt="Até 30% de desconto em Pizzas!"
+          height={0}
+          width={0}
+          className="h-auto w-full object-contain"
+          sizes="100vw"
+          quality={100}
+        />
       </div>
     </>
   );
